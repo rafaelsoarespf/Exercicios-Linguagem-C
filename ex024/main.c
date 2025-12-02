6719715470faf0db3f7905358a9131adff048c69
Rafael Soares de Paula FIlho <rafaelsoares.p.f@gmail.com>
2025-12-01 22:04:25 -0300

    concluído exercício 23

---
+++ ex024/main.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int n,i, A; // declarando variáveis
+
+	  printf("Digite um numero: ");
+	  scanf("%d", &n);
+
+	  printf("=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	  printf("CALCULANDO...\n");
+	  printf("=-=-=-=-=-=-=-=-=-=-=-=-\n");
+
+	  for(i == 0; i<= n; i++){
+		  A = A + i;
+	  }
+	printf("Valor de A = %d\n", A);
+    return 0;
+}
